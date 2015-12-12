8c3c39a49c14669d400c2f61d0555fd7206c22a9
Stephan Broek <stephan@sbroek.com>
2015-12-12 21:14:15 +0100

    made setup more universal

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:system', function(req, res, next) {
+  res.render('sheets/' + req.params.system);
+});
+
 module.exports = router;
